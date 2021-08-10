0a7709a691ba0a4b4a4b2aa6b34395d2c689cd6b
TarouTanakaYokohama <57135586+TarouTanakaYokohama@users.noreply.github.com>
2021-08-10 10:54:09 +0900

    商品概要

--- opt/meiji/product.py
+++ opt/meiji/product.py
@@ -1,21 +1,26 @@
 from bs4 import BeautifulSoup
 from urllib import request
+import requests
 
 import re
 import time
 
-url = 'https://www.meiji.co.jp'
-cate = '/products/chocolate/'
-response = request.urlopen(url+cate)
-soup = BeautifulSoup(response)
-product_list =[]
+# url = 'https://www.meiji.co.jp'
+# cate = '/products/chocolate/'
+# response = request.urlopen(url+cate)
+# soup = BeautifulSoup(response)
+# url_items = soup.select('.l-grid-row a')
+# product_list =[]
 
-url_items = soup.select('.l-grid-row a')
+# for a in url_items:
+#     test = a['href']
+#     eq = test.count('.html')
+#     if(eq == 1):
+#         url_list = url + test
+#         product_list.append(url_list)
 
-for a in url_items:
-    test = a['href']
-    eq = test.count('.html')
-    if(eq == 1):
-        url_list = url + test
-        product_list.append(url_list)
-
+aa = requests.get('https://www.meiji.co.jp/products/chocolate/04777.html')
+Beautiful = BeautifulSoup(aa.content,"html.parser")
+product = Beautiful.find_all('tr')
+word = [x.text.replace('\n',' ') for x in product]
+print(word)
