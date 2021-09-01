af9b132d350574c66870dcfd5093134439fddc45
TarouTanakaYokohama <57135586+TarouTanakaYokohama@users.noreply.github.com>
2021-09-01 14:19:01 +0900

    brandの数字を修正

--- opt/meiji/brand_up.py
+++ opt/meiji/brand_up.py
@@ -7,6 +7,8 @@ import datetime
 
 choco_brand_list = ['','ミルクチョコレート','明治 ザ・チョコレート','アーモンドチョコレート','マカダミアチョコレート','その他ナッツチョコレート','きのこの山','たけのこの里','きのこの山とたけのこの里','チョコレート効果','オリゴスマート','明治TANPACT','エムズバー','ガルボ','フラン','ホルン','プッカ','アグロフォレストリーミルクチョコレート','MyチョコBox','小粒チョコ','リッチチョコサンド']
 
+brand_count = len(choco_brand_list)
+
 # Use a service account
 cred = credentials.Certificate('../umyfoods-rac-firebase-adminsdk-m6vos-476571680f.json')
 firebase_admin.initialize_app(cred)
@@ -15,10 +17,10 @@ db = firestore.client()
 dat = string.digits + string.ascii_lowercase + string.ascii_uppercase
 rand = ''.join([random.choice(dat) for i in range(19)])
 
-number = 21
+number = 0
 
-doc_ref = db.collection(u'brandaaa').document(rand)
+doc_ref = db.collection(u'brand').document('rand')
 doc_ref.set({
-    u'brand_id': u'a',
-    u'brand_name': u'a'
+    u'brand_id': number+brand_count,
+    u'brand_name': choco_brand_list[number]
 })
