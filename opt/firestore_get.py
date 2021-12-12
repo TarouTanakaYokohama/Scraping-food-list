066b07b054727536c4874543090ceee9cb532968
TarouTanakaYokohama <57135586+TarouTanakaYokohama@users.noreply.github.com>
2021-12-12 10:32:34 +0900

    追加

--- opt/firestore_get.py
+++ opt/firestore_get.py
@@ -11,13 +11,35 @@ db = firestore.client()
 # query = docs.order_by("brand_id").limit_to_last(2)
 # query = docs
 # docs_list = docs.get()
-# Import database module.
-# from firebase_admin import db
 
 db = firestore.client()
-docs = db.collection('major_category').get()
-for doc in docs:
-    print(doc.to_dict())
+# docs = db.collection('major_category').get()
+# for doc in docs:
+#     print(doc.to_dict())
+Allergie = []
+aaa = '018'
+
+fafafa = '003'
+for i in range(3):
+    if i <10:
+        Allergie.append('00'+str(i))
+    else :
+        Allergie.append('0'+str(i))
+    doc_ref = db.collection('major_category').document(aaa)
+    doc_ref.collection(u'category').document(fafafa).collection(u'sub_category').document(str(Allergie[i])).set({
+          'category_id': str(Allergie[i])
+        },merge=True)
+    # doc_ref.set({
+    #     'category_id': str(Allergie[i])
+    #     },merge=True)
+
+# doc_ref = db.collection(u'major_category').document(u'001')
+
+# doc = doc_ref.get()
+# if doc.exists:
+#     print(f'Document data: {doc.to_dict()}')
+# else:
+#     print(u'No such document!')
 
 
 # for doc in docs_list:
